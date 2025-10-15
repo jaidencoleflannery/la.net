d6327b435c2e7bd2d52289cf64a1d9c4c3323a3b
Cole Flannery <jaidencoleflannery@gmail.com>
2025-10-14 21:19:20 -0500

    logic changes.

--- src/la.net/matrices/matrix.cs
+++ src/la.net/matrices/matrix.cs
@@ -3,123 +3,215 @@ using System.Numerics;
 using System.Reflection.Metadata;
 
 namespace Matrices;
-public sealed class Matrix<T> where T : INumber<T> {
+public sealed class Matrix<T> where T : INumber<T>
+{
     public int Cols { get; set; }
     public int Rows { get; set; }
     private T[,] _data;
-    public Matrix(int rows, int cols, T[,]? data = null) {
-        if(rows <= 0) { 
+    public Matrix(int rows, int cols, T[,]? data = null)
+    {
+        if (rows <= 0)
+        {
             throw new ArgumentOutOfRangeException(nameof(rows), $"{nameof(rows)} must be greater than 0.");
-        } else if(cols <= 0) { 
-            throw new ArgumentOutOfRangeException(nameof(cols), $"{nameof(cols)} must be greater than 0."); 
+        }
+        else if (cols <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(cols), $"{nameof(cols)} must be greater than 0.");
         }
 
-        if(data != null && rows < data.GetLength(0)) {
+        if (data != null && rows < data.GetLength(0))
+        {
+            Console.WriteLine(data.GetLength(0));
             throw new ArgumentOutOfRangeException(nameof(rows), $"{nameof(rows)} must be greater than or equal to provided data.");
-        } else if(data != null && cols < data.GetLength(1)) {
+        }
+        else if (data != null && cols < data.GetLength(1))
+        {
+            Console.WriteLine(data.GetLength(1));
             throw new ArgumentOutOfRangeException(nameof(cols), $"{nameof(cols)} must be greater than or equal to provided data.");
         }
 
+        Console.WriteLine($"{cols}, {rows}");
         this.Cols = cols;
         this.Rows = rows;
 
         _data = new T[rows, cols];
 
-        if(data == null) {
+        if (data == null)
+        {
             int max;
             // we are traversing the diagonal path of the matrix and need to know where the matrix ends, max is where the final pivot value will lay despite any free variables
-            if(rows >= cols) {
+            if (rows >= cols)
+            {
                 max = cols;
-            } else {
+            }
+            else
+            {
                 max = rows;
             }
             // this turns our zero matrix into an identity matrix
-            for(int cursor = 0; cursor < max; cursor++) { 
+            for (int cursor = 0; cursor < max; cursor++)
+            {
                 _data[cursor, cursor] = T.One;
             }
-        } else {
+        }
+        else
+        {
             // push smaller dimensional matrix into larger dimensional identity matrix
-            if(data.GetLength(0) < rows || data.GetLength(1) < cols) {
-                for(int row = 0; row < data.GetLength(0); row++) {
-                    for(int col = 0; col < data.GetLength(1); col++) {
-                            _data[row, col] = data[row, col];
+            if (data.GetLength(0) < rows || data.GetLength(1) < cols)
+            {
+                for (int row = 0; row < data.GetLength(0); row++)
+                {
+                    for (int col = 0; col < data.GetLength(1); col++)
+                    {
+                        _data[row, col] = data[row, col];
                     }
                 }
                 int index = (data.GetLength(0) > data.GetLength(1)) ? data.GetLength(1) : data.GetLength(0);
                 int max;
                 // we are traversing the diagonal path of the matrix PAST the provided matrix and turning it into an identity matrix
-                if(rows >= cols) {
+                if (rows >= cols)
+                {
                     max = rows;
-                } else {
+                }
+                else
+                {
                     max = cols;
                 }
-                for(int cursor = index; cursor < max; cursor++) {
-                    if(cursor < _data.GetLength(0) && cursor < _data.GetLength(1)) {
+                for (int cursor = index; cursor < max; cursor++)
+                {
+                    if (cursor < _data.GetLength(0) && cursor < _data.GetLength(1))
+                    {
                         _data[cursor, cursor] = T.One;
-                    } else {
+                    }
+                    else
+                    {
                         break;
                     }
                 }
-            } else {
+            }
+            else
+            {
                 _data = (T[,])data.Clone();
             }
         }
     }
 
-    public T Get(int row, int col) {
+    public T Get(int row, int col)
+    {
         return _data[row, col];
     }
 
-    public T[,] GetRow(int row) {
-        T[,] output = new T[1, Cols]; 
-        for(int col = 0; col < Cols; col++) {
-            output[0, col] = _data[row, col];
+    public T[] GetRow(int row)
+    {
+        if (row < 0 || row >= Rows)
+        {
+            throw new ArgumentOutOfRangeException(nameof(row), "Row index is out of range.");
+        }
+        T[] output = new T[Cols];
+        for (int col = 0; col < Cols; col++)
+        {
+            output[col] = _data[row, col];
         }
         return output;
     }
 
-    public void Set(int row, int col, T value) {
+    public void Set(int row, int col, T value)
+    {
         _data[row, col] = value;
     }
 
-    public void SetRow(int row, T[] value) {
-        for(int col = 0; col < value.Length; col++) {
-            _data[row, col] = value[col];
+    public void SetRow(int row, T[] value, Boolean conform)
+    {
+        if(Cols < value.Length) {
+            if(conform) {
+                throw new ArgumentOutOfRangeException(nameof(row), "Added row must be less than or equal to column length of existing matrix.");
+            } else {
+                throw new ArgumentOutOfRangeException(nameof(row), "Added row must be equal to column length of existing matrix.");
+            }
+        }
+        for (int col = 0; col < Cols; col++) {
+            if(col < value.Length) {
+                _data[row, col] = value[col];
+            } else {
+                _data[row, col] = T.Zero;
+            }
+        }
+        if(conform) {
+            // convert n*n index to identity form
+            if(Cols > value.Length) {
+                if(row > value.Length) {
+                    _data[row, row] = T.One;
+                }
+            }
         }
     }
 
-    public void PushRow(T[] value) {
-        if(value.Length != Cols) {
-            throw new ArgumentOutOfRangeException(nameof(Rows), $"{nameof(Rows)} must match existing matrix's length."); 
+    public void PushRow(T[] value, Boolean conform)
+    {
+        if(Cols < value.Length) {
+            if(conform) {
+                throw new ArgumentOutOfRangeException(nameof(value), "Added row must be less than or equal to column length of existing matrix.");
+            } else {
+                throw new ArgumentOutOfRangeException(nameof(value), "Added row must be equal to column length of existing matrix.");
+            }
         }
         T[,] matrix = new T[Rows + 1, Cols];
-        for(int col = 0; col < Cols; col++) {
+        for (int col = 0; col < value.Length; col++)
+        {
             matrix[0, col] = value[col];
         }
-        for(int row = 0; row < Rows; row++) {
-            for(int col = 0; col < Cols; col++) {
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Cols; col++)
+            {
                 matrix[row + 1, col] = _data[row, col];
+            }
+        }
+        if(conform) {
+            // convert n*n index to identity form
+            if(value.Length == 0) {
+                    matrix[0, 0] = T.One;
             }
         }
         Rows += 1;
         _data = matrix;
     }
 
-    public void AppendRow(T[] value) {
+    public void AppendRow(T[] value, Boolean conform)
+    {
+        if(Cols < value.Length) {
+            if(conform) {
+                throw new ArgumentOutOfRangeException(nameof(value), "Added row must be less than or equal to column length of existing matrix.");
+            } else {
+                throw new ArgumentOutOfRangeException(nameof(value), "Added row must be equal to column length of existing matrix.");
+            }
+        }
+        T[,] matrix = new T[Rows, Cols + 1];
+        for(int col = 0; col < value.Length; col++) {
+            matrix[Rows, col] = value[col];
+        }
+        if(conform) {
+            // convert n*n index to identity form
+            if(value.Length < Rows) {
+                matrix[Rows, Rows] = T.One;
+            }
+        }
+        _data = matrix;
         this.Cols++;
-        Matrix<T> tempMatrix = new (Rows, Cols, _data);
-        tempMatrix.SetRow(Cols, value);
     }
 
-    public (int Rows, int Cols) GetSize() {
-        return ( Rows: _data.GetLength(0), Cols: _data.GetLength(1) ) ;
+    public (int Rows, int Cols) GetSize()
+    {
+        return (Rows: _data.GetLength(0), Cols: _data.GetLength(1));
     }
 
-    public string Print() {
+    public string Print()
+    {
         string output = "";
-        for(int row = 0; row < Rows; row++) {
+        for (int row = 0; row < Rows; row++)
+        {
             output += "| ";
-            for(int col = 0; col < Cols - 1; col++) { output += $"{_data[row, col]} "; }
+            for (int col = 0; col < Cols; col++) { output += $"{_data[row, col]} "; }
             output += "|\n";
         }
         return output;
