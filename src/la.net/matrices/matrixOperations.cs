be3f68f59a247e5f79c01dfba5081a4afbf3286c
Cole Flannery <jaidencoleflannery@gmail.com>
2025-12-05 22:22:40 -0600

    fixed logic on swaprows() and findpivot()

--- src/la.net/matrices/matrixOperations.cs
+++ src/la.net/matrices/matrixOperations.cs
@@ -22,12 +22,17 @@ static class MatrixOperations {
         }
     }
 
-    private static (int row, int col) FindPivot<T>(this Matrix<T> instance, int initRow) where T : INumber<T> {
-        if(initRow == instance.Rows) {
+    public static (int row, int col) FindPivot<T>(this Matrix<T> instance, int initRow) where T : INumber<T> {
+        if(initRow >= instance.Rows) {
             throw new ArgumentOutOfRangeException(nameof(initRow), $"{nameof(initRow)} must be less than the total number of rows.");
         }
-        int[] numZeroes = [];
+        int[] numZeroes = new int[instance.Rows];
         int pivotRow = 0;
+        // if the first row has a leading value, then we can call it the pivot
+        if(instance.Get(initRow, 0) != T.Zero) {
+            return (initRow, 0);
+        }
+        // else let's find the row with the least leading zeroes
         for(int row = initRow; row < instance.Rows; row++) {
             for(int col = 0; col < instance.Cols; col++) {
                 if(instance.Get(row, col) == T.Zero) {
@@ -37,19 +42,19 @@ static class MatrixOperations {
                 }
             }
         }
-        for(int cursor = 1; cursor < (numZeroes.Length - 1); cursor++) {
+        for(int cursor = 0; cursor < numZeroes.Length; cursor++) {
             if(numZeroes[cursor] < numZeroes[pivotRow]) {
                 pivotRow = cursor;
             }
         }
         // numZeroes.Length will be the first index after leading zeroes (pivot)
-        return (pivotRow, numZeroes.Length);
+        return (pivotRow, numZeroes[pivotRow]);
     }
 
     private static void reduceRow<T>(this Matrix<T> instance, (int row, int col) swap, (int row, int col) pivot) where T : INumber<T>{
-        T scalar = -instance.Get(pivot.row, pivot.col) / instance.Get(swap.row, swap.col);
+        T scalar = -instance.Get(swap.row, swap.col) / instance.Get(pivot.row, pivot.col);
         for(int cursor = 0; cursor < instance.Rows; cursor++) {
-            T value = instance.Get(swap.row, swap.col) - scalar * instance.Get(pivot.row, cursor);
+            T value = instance.Get(swap.row, swap.col) + scalar * instance.Get(pivot.row, cursor);
             instance.Set(swap.row, swap.col, value);
         }
     }
