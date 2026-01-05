fe88cc9467ea695426346379cbc4f0013cc572d8
Cole Flannery <jaidencoleflannery@gmail.com>
2026-01-05 00:14:36 -0600

    matrixtests | test coverage for matrix constructor.

--- testing/Matrices/MatrixTests.cs
+++ testing/Matrices/MatrixTests.cs
@@ -18,14 +18,29 @@ public class MatrixTests
 
         Assert.Equal(matrix.Rows, rows);
         Assert.Equal(matrix.Cols, cols);
-    }
+    } 
 
     [Theory]
     [InlineData(1, 3)]
     [InlineData(3, 1)]
+    [InlineData(4, 0)]
+    [InlineData(0, 4)]
+    [InlineData(0, 0)]
+    public void Ctor_DoesntSetTooSmallRowsAndCols_Fail(int rows, int cols) {
+        var data = new double[,] {
+            {0, 4, 8}, 
+            {0, 0, 8},
+            {0, 1, 8},
+        };
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Matrix<double>(rows, cols, data));
+    }
+
+    [Theory]
     [InlineData(4, -3)]
-    [InlineData(14, 0)]
-    public void Ctor_DoesntSetBadRowsAndCols_Fail(int rows, int cols) {
+    [InlineData(-4, 3)]
+    [InlineData(-4, -3)]
+    public void Ctor_DoesntSetNegativeRowsAndCols_Fail(int rows, int cols) {
         var data = new double[,] {
             {0, 4, 8}, 
             {0, 0, 8},
@@ -37,15 +52,54 @@ public class MatrixTests
 
     [Fact]
     public void Ctor_WithData_StoresValuesProperly_Success() {
-        Matrix<double> matrix = new Matrix<double>(3, 3,
-            new double[,] {
+        var data = new double[,] {
                 {0, 4, 8}, 
-                {0, 0, 8},
+                {0, 3, 8},
                 {0, 1, 8},
-            }
-        );
+            };
 
-        Assert.Equal((matrix.Get(0, 1)), 4);
-        Assert.Equal((matrix.Get(2, 2)), 8);
+        Matrix<double> matrix = new Matrix<double>(3, 3, data);
+
+        for(int row = 0; row < matrix.Rows; row++) {
+            for(int col = 0; col < matrix.Cols; col++) {
+                Assert.Equal((matrix.Get(row, col)), data[row, col]);
+            }
+        }
+    }
+
+    [Fact]
+    public void Ctor_NoData_CreatesIdentityMatrixProperly_Success() {
+        Matrix<double> matrix = new Matrix<double>(3, 3);
+
+        for(int row = 0; row < matrix.Rows; row++) {
+            for(int col = 0; col < matrix.Cols; col++) {
+                if(row == col) Assert.Equal((matrix.Get(row, col)), 1);
+                if(row != col) Assert.Equal((matrix.Get(row, col)), 0);
+            }
+        }
+    }
+
+    [Fact]
+    public void Ctor_WithData_MatrixConforms_Success() {
+        var data =  new double[,] {
+                {0, 4, 8}, 
+                {0, 3, 8},
+                {0, 1, 8},
+            };
+
+        Matrix<double> matrix = new Matrix<double>(5, 5, data);
+
+        for(int row = 0; row < 3; row++) {
+            for(int col = 0; col < 3; col++) {
+                Assert.Equal((matrix.Get(row, col)), data[row, col]);
+            }
+        }
+
+        for(int row = 3; row < matrix.Rows; row++) {
+            for(int col = 3; col < matrix.Cols; col++) {
+                if(row == col) Assert.Equal((matrix.Get(row, col)), 1);
+                if(row != col) Assert.Equal((matrix.Get(row, col)), 0);
+            }
+        }    
     }
 }
